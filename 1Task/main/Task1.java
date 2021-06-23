a306b1c5ebb89b26c81594386813c850c6814ecc
korolenko <korolenko.as1204@asugubkin.ru>
2021-06-23 14:05:06 +0300

    1Task added

--- 1Task/main/Task1.java
+++ 1Task/main/Task1.java
@@ -7,17 +7,21 @@ public class Task1 {
      * not effictive O(n^2) solution
      */
     private static void oN2(String testString){
-        for (int i = 0, n = testString.length(); i < n; i++) {
-            char c = testString.charAt(i);
-            int mediumMax = 0;
-            for(int j = 0, m = testString.length(); j < m; j++){
-                char d = testString.charAt(j);
-                if(d == c){
-                    mediumMax ++;
+        if(testString == null){
+            res = 0;
+        } else {
+            for (int i = 0, n = testString.length(); i < n; i++) {
+                char c = testString.charAt(i);
+                int mediumMax = 0;
+                for (int j = 0, m = testString.length(); j < m; j++) {
+                    char d = testString.charAt(j);
+                    if (d == c) {
+                        mediumMax++;
+                    }
                 }
-            }
-            if(mediumMax > res){
-                res = mediumMax;
+                if (mediumMax > res) {
+                    res = mediumMax;
+                }
             }
         }
         System.out.println(res);
@@ -27,26 +31,30 @@ public class Task1 {
      * effictive O(n*k) solution
      */
     private static void oN(String testString){
-        HashMap<Character,Integer> dictionary = new HashMap<>();
-        char [] strArray = testString.toCharArray();
-        for (char elem:strArray) {
-            if (!dictionary.containsKey(elem)) {
-                dictionary.put(elem, 1);
-            } else {
-                int val = dictionary.get(elem);
-                dictionary.put(elem, val + 1);
+        if(testString == null){
+            res = 0;
+        } else {
+            HashMap<Character, Integer> dictionary = new HashMap<>();
+            char[] strArray = testString.toCharArray();
+            for (char elem : strArray) {
+                if (!dictionary.containsKey(elem)) {
+                    dictionary.put(elem, 1);
+                } else {
+                    int val = dictionary.get(elem);
+                    dictionary.put(elem, val + 1);
+                }
             }
-        }
-        res = 0;
-        for (Integer val:dictionary.values()){
-            if(val>res){
-                res = val;
+            res = 0;
+            for (Integer val : dictionary.values()) {
+                if (val > res) {
+                    res = val;
+                }
             }
         }
         System.out.println(res);
     }
     public static void main(String[] args) {
-        String testString = "abbaac";
+        String testString = null;
         oN2(testString);
         oN(testString);
     }
